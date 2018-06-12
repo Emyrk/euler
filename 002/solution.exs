b30865962f739aab6f96cc4048bf426b5e0313c3
Emyrk <stevenmasley@gmail.com>
2018-06-12 12:26:57 -0400

    Aim for tail recursion

--- 002/solution.exs
+++ 002/solution.exs
@@ -3,14 +3,15 @@
 
 defmodule Fib do
   # 4 mil limit
-  def fib(a, b) when a > 4_000_000 or b > 4_000_000 do
-    [a, b]
+  def fib(acc, a, b) when a > 4_000_000 or b > 4_000_000 do
+    acc ++ [a]
   end
 
-  def fib(a, b) do
-    [a] ++ fib(b, a + b)
+  def fib(acc, a, b) do
+    fib(acc ++ [a], b, a + b)
   end
 end
 
 even? = &(rem(&1, 2) == 0)
-Fib.fib(1, 2) |> Enum.filter(even?) |> Enum.sum()
+# 4613732
+Fib.fib([], 1, 2) |> Enum.filter(even?) |> Enum.sum()
