cea09d920bc1fd2c136e914b01a18915b2e19711
emyrk <stevenmasley@gmail.com>
2018-06-12 20:46:52 -0400

    Change accumulator to integer

--- 002/solution.exs
+++ 002/solution.exs
@@ -2,16 +2,22 @@
 #   Fib sequence sum of even
 
 defmodule Fib do
+  def even?(v), do: rem(v, 2) == 0
+
   # 4 mil limit
-  def fib(acc, a, b) when a > 4_000_000 do
-    acc ++ [a]
+  def fib(acc, a, _) when a > 4_000_000 do
+    acc
   end
 
   def fib(acc, a, b) do
-    fib(acc ++ [a], b, a + b)
+    next = a + b
+    if even?(next) do
+      fib(acc + next, b, next)
+    else
+      fib(acc, b, next)
+    end
   end
 end
 
-even? = &(rem(&1, 2) == 0)
 # 4613732
-Fib.fib([], 1, 2) |> Enum.filter(even?) |> Enum.sum()
+Fib.fib(0, 1, 2)
