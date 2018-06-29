5510489b6f7e3cd970ba9d9745f7a1f9fdebd9d0
emyrk <stevenmasley@gmail.com>
2018-06-29 19:54:11 -0400

    Problem 13 completed

---
+++ 013/solutions.exs
@@ -0,0 +1,16 @@
+answer = File.stream!("numbers.txt")
+|> Stream.map(fn s ->
+    {v, _} =Integer.parse(s)
+        v
+end) 
+|> Enum.reduce(0, fn v, acc ->
+    acc + v
+end)
+
+# First 10 digits
+answer = Integer.digits(answer) |> Enum.take(10) |> Integer.undigits
+IO.puts answer
+
+# Answer
+# 5537376230390876637302048746832985971773659831892672
+# 5537376230
