59ca40ad4ce6860135813d3cdd8082e76c55a3ac
Emyrk <stevenmasley@gmail.com>
2018-07-02 18:17:59 -0400

    Remove the if/else

--- 002/simple.exs
+++ 002/simple.exs
@@ -1,5 +1,5 @@
-# As little lines as is reasonable. 
-fib = &(if &1 < trunc(4.0e6) do
-        if rem(&2, 2) == 0, do: &2 + &3.(&2, &1 + &2, &3), else: &3.(&2, &1 + &2, &3)
-    else; 0; end)
+fib = &(cond do
+         &1 < trunc(4.0e6)-> if rem(&2, 2) == 0, do: &2 + &3.(&2, &1 + &2, &3), else: &3.(&2, &1 + &2, &3)
+        true -> 0 
+    end)
 IO.puts fib.(1, 2, fib)
