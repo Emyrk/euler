e971deb5ea33a21f4c2d571f80698a68d72ecc5e
Emyrk <stevenmasley@gmail.com>
2018-07-02 17:28:36 -0400

    Reduced line count

--- 002/main.go
+++ 002/main.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// Fibonacci
-	fib := []int{1, 2}
-	sum := 2
-	for {
-		next := fib[0] + fib[1]
-		fib[0], fib[1] = fib[1], next
-		if next%2 == 0 {
-			sum += next
-		}
-		if next > 4e6 {
-			break
+	fib, sum := []int{1, 2}, 2
+	for fib[1] < 4e6 {
+		fib[0], fib[1] = fib[1], fib[0]+fib[1]
+		if fib[1]%2 == 0 {
+			sum += fib[1]
 		}
 	}
-
 	fmt.Println(sum)
 }
