7d7814032e67f1f0f72e0ff7d30a2d5429528c7f
Emyrk <stevenmasley@gmail.com>
2018-07-02 21:12:50 -0400

    Start 11

--- 011/main.go
+++ 011/main.go
@@ -8,10 +8,42 @@ import (
 )
 
 func main() {
-	get_grid()
+	grid := GetGrid()
+	WorstBruteForce(grid)
 }
 
-func get_grid() [][]int {
+func WorstBruteForce(grid [][]int) {
+	grid = expandGrid(grid)
+	//largestProduct := 0
+	for i := 3; i < len(grid)-3; i++ {
+		for i := 3; i < len(grid[i])-3; i++ {
+
+		}
+	}
+}
+
+func expandGrid(grid [][]int) [][]int {
+	// Add 3 cols and 3 rows to all sides
+	rowLength := len(grid[0])
+	blankRow := make([]int, rowLength+6)
+	for i := range grid {
+		grid[i] = append([]int{1, 1, 1}, grid[i]...)
+		grid[i] = append(grid[i], []int{1, 1, 1}...)
+	}
+
+	grid = append([][]int{blankRow, blankRow, blankRow}, grid...)
+	grid = append(grid, [][]int{blankRow, blankRow, blankRow}...)
+	return grid
+}
+
+func ComputeLargestProduct(grid [][]int, i, j int) {
+	// Check products vertically
+	for i := range grid {
+
+	}
+}
+
+func GetGrid() [][]int {
 	file, err := os.OpenFile("grid.txt", os.O_RDONLY, 0777)
 	if err != nil {
 		panic(err)
