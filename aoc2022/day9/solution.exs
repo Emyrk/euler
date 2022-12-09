bd0c59f6c8e71c02bca5f4d1d6996a868c079463
Steven Masley <stevenmasley@coder.com>
2022-12-09 10:56:54 -0600

    Day 9

--- aoc2022/day9/solution.exs
+++ aoc2022/day9/solution.exs
@@ -1,58 +1,32 @@
-defmodule Chain do
-  defstruct Ropes: []
-
-  def new(n) do
-    %Chain{Ropes: Enum.map(1..n, fn _ -> %Rope{} end)}
-  end
-
-  def simulate(chain, moves) do
-    moves
-    |> Enum.map(&String.trim(&1, "\n"))
-    |> Enum.map(&String.split(&1, " "))
-    |> Enum.reduce(chain, fn [direction, num], acc ->
-      moves(acc, direction, String.to_integer(num))
-    end)
-  end
-
-  def moves(chain, direction, n) do
-    # IO.puts("Moving #{n} times in direction #{direction}")
-
-    Enum.reduce(1..n, chain, fn _, acc ->
-      move(acc, direction)
-    end)
-  end
-
-  def move(chain, direction) do
-    moved =
-      chain.Ropes
-      |> Enum.with_index()
-      |> Enum.reduce(%{ropes: [], head: nil}, fn {rope, index}, acc ->
-        case head do
-          nil ->
-            rope = Rope.move(rope, direction)
-
-            %{
-              ropes: acc.ropes ++ [rope],
-              head: rope.tail
-            }
-
-          _ ->
-            rope = Map.put(rope, :head, acc.head)
-            rope = Rope.move(rope, direction)
-
-            %{
-              ropes: acc.ropes ++ [rope],
-              head: rope.head
-            }
-        end
-      end)
-
-    %Chain{ropes: moved.ropes}
-  end
-end
+require IEx
 
 defmodule Rope do
   defstruct head: {0, 0}, tail: {0, 0}, visited: %{{0, 0} => true}
+
+  def bounds(rope) do
+    Map.put(rope.visited, rope.head, false)
+    |> Enum.reduce({0, 0, 0, 0}, fn {{x, y}, _}, {minx, miny, maxx, maxy} ->
+      {min(minx, x), min(miny, y), max(maxx, x), max(maxy, y)}
+    end)
+  end
+
+  def draw(rope) do
+    {minx, miny, maxx, maxy} = Rope.bounds(rope)
+
+    Enum.each(maxy..miny, fn y ->
+      Enum.each(minx..maxx, fn x ->
+        cond do
+          {x, y} == rope.tail -> IO.write("T")
+          Map.has_key?(rope.visited, {x, y}) -> IO.write("#")
+          {x, y} == rope.head -> IO.write("H")
+          true -> IO.write(".")
+        end
+      end)
+
+      IO.puts("")
+      IO.puts("-")
+    end)
+  end
 
   def moves(rope, direction, n) do
     # IO.puts("Moving #{n} times in direction #{direction}")
@@ -62,30 +36,10 @@ defmodule Rope do
     end)
   end
 
-  def move(rope, direction) do
-    # Move the head into the direction
-    head =
-      case direction do
-        "U" ->
-          {x, y} = rope.head
-          {x, y + 1}
-
-        "D" ->
-          {x, y} = rope.head
-          {x, y - 1}
-
-        "L" ->
-          {x, y} = rope.head
-          {x - 1, y}
-
-        "R" ->
-          {x, y} = rope.head
-          {x + 1, y}
-      end
-
+  def updateTail(rope) do
     # Move the tail towards the head
     {tx, ty} = rope.tail
-    {hx, hy} = head
+    {hx, hy} = rope.head
 
     dx = hx - tx
     dy = hy - ty
@@ -119,10 +73,38 @@ defmodule Rope do
     # IO.puts("Head: {#{hx}, #{hy}} Tail: {#{newx}, #{newy}}")
 
     %Rope{
-      head: head,
+      head: rope.head,
       tail: {newx, newy},
       visited: Map.put(rope.visited, {newx, newy}, true)
     }
+  end
+
+  def move(rope, direction) do
+    # Move the head into the direction
+    head =
+      case direction do
+        "U" ->
+          {x, y} = rope.head
+          {x, y + 1}
+
+        "D" ->
+          {x, y} = rope.head
+          {x, y - 1}
+
+        "L" ->
+          {x, y} = rope.head
+          {x - 1, y}
+
+        "R" ->
+          {x, y} = rope.head
+          {x + 1, y}
+      end
+
+    Rope.updateTail(%Rope{
+      head: head,
+      tail: rope.tail,
+      visited: rope.visited
+    })
   end
 
   def simulate(moves) do
@@ -135,12 +117,69 @@ defmodule Rope do
   end
 end
 
+defmodule Chain do
+  defstruct ropes: []
+
+  def new(n) do
+    %Chain{ropes: Enum.map(1..n, fn _ -> %Rope{} end)}
+  end
+
+  def simulate(chain, moves) do
+    moves
+    |> Enum.map(&String.trim(&1, "\n"))
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.reduce(chain, fn [direction, num], acc ->
+      moves(acc, direction, String.to_integer(num))
+    end)
+  end
+
+  def moves(chain, direction, n) do
+    # IO.puts("Moving #{n} times in direction #{direction}")
+
+    Enum.reduce(1..n, chain, fn _, acc ->
+      move(acc, direction)
+    end)
+  end
+
+  def move(chain, direction) do
+    moved =
+      chain.ropes
+      |> Enum.with_index()
+      |> Enum.reduce(%{ropes: [], head: nil}, fn {rope, index}, acc ->
+        # IEx.pry()
+
+        case acc.head do
+          nil ->
+            rope = Rope.move(rope, direction)
+
+            %{
+              ropes: acc.ropes ++ [rope],
+              head: rope.tail
+            }
+
+          _ ->
+            # IEx.pry()
+            rope = Map.put(rope, :head, acc.head)
+            rope = Rope.updateTail(rope)
+
+            %{
+              ropes: acc.ropes ++ [rope],
+              head: rope.tail
+            }
+        end
+      end)
+
+    %Chain{ropes: moved.ropes}
+  end
+end
+
 rope = Rope.simulate(File.stream!("input.txt"))
 IO.puts("Part 1 Visited: #{Map.size(rope.visited)}")
 
-chain = Chain.new(10)
+chain = Chain.new(9)
 chain = Chain.simulate(chain, File.stream!("input.txt"))
-IO.puts("Part 1 Visited: #{Map.size(Enum.at(chain.ropes, 9).visited)}")
+IO.puts("Part 2 Visited: #{Map.size(Enum.at(chain.ropes, 8).visited)}")
+# Rope.draw(Enum.at(chain.ropes, 8))
 
 defmodule Expects do
   def testDiagonal() do
